9835192371f5f0e23b20ffce128e4d4912b88507
Andres <131395057+AndresJimenezS@users.noreply.github.com>
2023-10-29 18:46:11 -0600

    Creacion proyecto

---
+++ src/main/java/com/paws4home/Paws4homeApplication.java
@@ -0,0 +1,13 @@
+package com.paws4home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paws4homeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paws4homeApplication.class, args);
+	}
+
+}
